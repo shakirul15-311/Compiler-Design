2630d2d2f90dd022d5a53601959a0e95a441c65e
Shakirul Islam <shakirul15-311@diu.edu.bd>
2017-10-26 01:48:57 +0600

    Create 1.Star line.cpp

---
+++ Assignment/1.Star line.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i,j;
+    printf("\n");
+    scanf("%d",&j);
+
+    for (i=1;i<=j;i++)
+        printf("*");
+
+    //getch();
+    return 0;
+}
