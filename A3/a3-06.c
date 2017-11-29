222a779cc590580b3091c4da03aa151ddbfed07e
Shakirul Islam <shakirul15-311@diu.edu.bd>
2017-11-30 00:16:09 +0600

    Update a3-06.c

--- A3/a3-06.c
+++ A3/a3-06.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 int main()
 {
-    int array[5], i;
+    int array[5], i, search, match = 0;
     for(i = 0; i < 5; i++)
     {
         scanf("%d", &array[i]);
     }
+    scanf("%d", &search);
     printf("\n");
     for(i = 0; i < 5; i++)
     {
-        if(array[i] < 10)
+        if(array[i] == search)
         {
-            printf("%d\t", array[i]);
+            printf("%d was found at location %d", search, (i + 1));
+            match = 1;
+            break;
         }
     }
 
+    if (match == 0)
+
+        printf("%d was not found", search);
+
     return 0;
 }
+
+
+
